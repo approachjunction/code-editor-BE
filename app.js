5d76ad3e57160dc7f5d37c804bb8431f59d26fbe
Rajneesh Mishra <127204999+razmishra@users.noreply.github.com>
2023-12-19 15:36:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
+app.use("/",router);
 
 
 app.listen(3001, () => {
