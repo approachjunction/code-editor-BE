cfd8aa41e35178858608efc60a1f76387db0dbcb
Rajneesh Mishra <rajneesh.solulab@gmail.com>
2023-12-21 11:49:52 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = require("./router/router");
 const cors = require("cors");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/",router);
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 
-app.listen(3001, () => {
-  console.log("app started at 3001");
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`app started at ${port}`);
 });
